07c02ba8e158ea93338986f990c694bd4959f081
Rory Hartong-Redden <rory@gmail.com>
2024-10-10 14:24:48 -0600

    Set up base.html

--- strava_deck/settings.py
+++ strava_deck/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["strava-deck.fly.dev", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["strava-deck.fly.dev", "127.0.0.1", "localhost", "stravadeck.com"]
 
 
 # Application definition
